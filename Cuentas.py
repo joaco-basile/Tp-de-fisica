2c61b0107b60a7d71f5ca353a59eea0a5dd23aaa
joaco-basile <81160492+joaco-basile@users.noreply.github.com>
2021-06-03 20:09:07 -0300

    Update Cuentas.py

--- Cuentas.py
+++ Cuentas.py
@@ -1,4 +1,3 @@
-
 import math
 
 #constantes
@@ -7,8 +6,9 @@ k = 9*10**9
 #Lados
 a = 0
 b = 5
-
 c = (float(input("ingresar distancia de la particula: ")))
+
+
 
 #angulos
 A = math.radians(50)
@@ -20,10 +20,9 @@ q1 = float(input("Escriba la carga 1: "))
 q2 = float(input("Escriba la carga 2: "))
 q3 = float(input("Escriba la carga 3: "))
 
-#fuerza sobre carga 2
 
 #teorema de coseno para el lado
-a = (b**2-2*b*c*math.cos(A)+c**2)**0.5
+a = math.sqrt(((b**2) - 2*b*c*math.cos(A) + (c**2) ))
 
 
 #Teorema de coseno y de pitagoras para los angulos
@@ -34,25 +33,25 @@ B = math.degrees(math.acos((0.5 * a**2 - 0.5 * b**2 + 0.5 * c**2) / (a*c)))
 A = math.degrees(A)
 C = 180 - (A + B) 
 
-#Pasamos la distancia a m
-c = c/1000
-a = a/100
 
 
 # Calculamos fuerzas totales
-f12 = round((k * ((q1*10**-6 * q2*10**-6) / c**2)),2)
+f12 = round((k * ((q1*10**-4 * q2*10**-4) / c**2)),2)
 f21 = f12 * -1
 
 
-f32 = round((k * ((q3*10**-6 * q2*10**-6) / a**2)),2)
-
+f32 = round((k * ((q3*10**-4 * q2*10**-6) / a**2)),2)
+f23 = f32 *-1
 
 # Calculamos fuerzas en x e y
 angulo_q3_int = 180 - (90 + 50)
 angulo_q3_ext = C - angulo_q3_int
 
-f32x = round((math.cos (angulo_q3_ext)) * fq3),2)
-f32y = round((math.sin (angulo_q3_ext)) * fq3),2)
+
+f32x = (math.cos (angulo_q3_ext)) * f32
+f32y = (math.sin (angulo_q3_ext)) * f32
+
+f2yt = f32y + f12
 
 f23x = f32x * -1
 f23y = f32y * -1
@@ -61,11 +60,19 @@ f23y = f32y * -1
 
 
 print("lado a :", a)
+print("lado b :", b)
+print("lado c :", c)
 print("angulo A", A)
 print("angulo B:", B)
 print("angulo C:", C)
+print("angulo angulo externo:", angulo_q3_ext)
 print("Fuerza f12:", f12)
 print("Fuerza f32", f32)
 print("Fuerza 32x", f32x)
 print("Fuerza 32y", f32y)
+print("Fuerza 32yt", f2yt)
+print("Fuerza f21:", f21)
+print("Fuerza f23", f23)
+print("Fuerza 23x", f23x)
+print("Fuerza 23y", f23y)
 
